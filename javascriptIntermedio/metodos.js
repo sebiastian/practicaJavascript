418979d5f1990d2482499bbc17edea3fba705e10
Sebastian Becerra <sebasbecerraph@gmail.com>
2025-06-13 11:24:32 -0300

    first commit

---
+++ javascriptIntermedio/metodos.js
@@ -0,0 +1,10 @@
+//Metodo Push
+let colores = ["Rojo", "Verde", "Azul"];
+console.log(colores);
+colores.push("Amarillo");
+console.log(colores);
+let otrosColores = ["negro"];
+otrosColores.unshift(colores);
+let nuevosColores = [];
+nuevosColores = otrosColores;
+console.log(nuevosColores);
